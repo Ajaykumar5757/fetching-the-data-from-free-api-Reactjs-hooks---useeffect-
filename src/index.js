7066eb3f751fd36339fa22cdba231077dd9cfd67
Ajaykumar5757 <ajaykumarak2703@gmail.com>
2024-08-17 11:55:50 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React, { useState,useEffect }  from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// useEffect
+
+function Example(){
+  const[count,setCount] = useState(0);
+  useEffect(()=>{setTimeout(()=>{setCount(count => count+1)},20000)});
+  return(
+    <h1>website counting detail {count} times</h1>
+  )
+}
+
+
+function Demo() {
+  const [users, setUser] = useState({}); 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setUser(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("Error loading data");
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div>
+      <h1>List out the user in API</h1>
+      <ul>
+      {users.map(user => (<li key={user.id}>{user.name}</li>))}
+      </ul>
+    </div>
+  )
+}
+
+function Final(){
+  return(
+    <div>
+      <Example/>
+      <Demo/>
+    </div>
+  )
+}
+
+ReactDOM.render(<Final/>,document.getElementById('root'));
